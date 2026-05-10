177442341f834de9fe55607acec6ff703a55af36
まろたつ <urimaro1108@gmail.com>
2026-05-10 22:11:48 +0900

    マップの解像度画悪すぎたため変更(ターニングポイント1)

--- Readme.md
+++ Readme.md
@@ -1,3 +1,16 @@
-仮想環境はMiniconda　mambaを想定
 
-また主の仮想環境名はriskmap_env
+未来の俺へ
+仮想環境はMiniconda mambeは現在のネットでは無理だったSSLエラー発生
+また、仮想環境名はriskmap_env
+
+    忘れていた時用手順：
+    1．クローン完了
+    2．Anaconda promptを開きymlファイルのあるローカルリポジトリまでcd
+    3．左端が(Base)ならconda env create -f create_env.yml
+    4．適宜ｙ
+    5．conda activate loveaudio_env
+    6．入れるのを確認したらconda deactivate
+    7．VSCでCtrl＋Shift＋PでPython: Select Interprinterを選択
+    8．実行環境(仮想環境)を洗濯
+    9．Ctrl＋Shift＋＠
+    10．python 実行ファイル.py
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib as plt
-import cartopy as ctp
+import matplotlib.pyplot as plt
+import cartopy.crs as ctp
 import contextily as cntx
 from pathlib import Path
 import json
@@ -11,10 +11,32 @@ json_data_file = current_dir / "data" / "data.json"
 
 print(json_data_file.name)
 
-json_open = open(json_data_file, 'r',encoding='utf-8')
-json_load = json.load(json_open)
+with open(json_data_file, 'r',encoding='utf-8') as json_open:
+    json_load = json.load(json_open)
 
-json_values = json_load['CASE1']['ID']
+json_values = json_load['CASE1']['lat']
 
 print(json_values)
 print(type(json_values))
+
+lats = []
+lons = []
+
+for case_data in json_load.values():
+    lats.append(case_data['lat'])
+    lons.append(case_data['lon'])
+
+print(lats)
+print(lons)
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(1,1,1,projection=ctp.Mercator())
+ax.scatter(lons,lats, color='red',s=50,transform=ctp.PlateCarree())
+margin = 0.002
+ax.set_extent([min(lons) - margin, max(lons) + margin,
+               min(lats) - margin, max(lats) +margin],
+               crs=ctp.PlateCarree())
+
+cntx.add_basemap(ax, zoom=15)
+
+plt.show()
